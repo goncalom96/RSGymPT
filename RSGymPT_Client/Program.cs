2051ebc4c23f53cd693b72c4cfccfb691ce71bec
Gonçalo Marques <goncalo-marques96@hotmail.com>
2024-12-20 10:39:50 +0000

    update

--- RSGymPT_Client/Program.cs
+++ RSGymPT_Client/Program.cs
@@ -7,37 +7,30 @@ namespace RSGymPT_Client
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
             Utility.SetUnicodeConsole();
 
             try
             {
+                // Dados iniciais:
+                // É suposto ser executado apenas uma vez
 
-                // ToDo: Descrevi apenas o método do Create do Client e Personal Trainer pois usei a mesma abordagem para os restantes
-                
-                // Dados iniciais estão comentados e ainda vou lançar
                 #region Starting Data
-                /*
                 UserRepository.StartingUsers();
                 LocationRepository.StartingLocations();
                 PersonalTrainerRepository.StartingPersonalTrainers();
                 ClientRepository.StartingClients();
                 RequestRepository.StartingRequests();
-                */
-                #endregion
+                #endregion Starting Data
 
                 LoginMenu.FirstMenu();
-
             }
             catch (Exception)
             {
-
             }
 
             Utility.TerminateConsole();
-
         }
     }
 }
